49a7a733b39404e6ae488cab8a95236ab67e4fce
t2n <t2n2004@gmail.com>
2023-11-16 22:32:04 +0700

    Tạo HTTP Server cơ bản

--- routes.js
+++ routes.js
@@ -4,13 +4,15 @@ const requestHandler = (req, res) => {
     const url = req.url;
     const method = req.method;
     if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
-        res.write('<head><title>Enter Message</title><head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
+        res.write('<head><title>Lab 2.1</title></head>');
+        res.write('<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end();
     }
-    if (url === '/message' && method === 'POST') {
+    if (url === '/create-user' && method === 'POST') {
         const body = [];
         req.on('data', (chunk) => {
           console.log(chunk);
@@ -18,18 +20,26 @@ const requestHandler = (req, res) => {
         });
         return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, (err) => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
+            console.log(parsedBody.split('=')[1]);
+            
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+            
         });
+    }
+
+    if (url === '/users') {
+        res.write('<html>');
+        res.write('<head><title>List of users</title></head>');
+        res.write('<body><ul><li>User 1</li><li>User 2</li></ul></body>');
+        res.write('</html>');
+        return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title><head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('<body><h1>This is Lab 2.1!</h1></body>');
     res.write('</html>');
     res.end();
 };

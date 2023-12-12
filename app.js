c99412a4af9f805a79419b147f69c00555f17e7d
t2n <t2n2004@gmail.com>
2023-12-12 22:16:20 +0700

    Lab4.2 Hiển thị file HTML lên trang Web

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const mainRoutes = require('./routes/index');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(mainRoutes);
 
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
